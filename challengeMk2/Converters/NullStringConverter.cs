d8898223d7550139964234c13352e2e79d8a7081
Amaury Dewaersegger <68230242+AmauryMi8@users.noreply.github.com>
2020-07-30 11:07:56 +0200

    Add Convertor
    add null string convertor

---
+++ challengeMk2/Converters/NullStringConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace challengeMk2.Converters
+{
+    public class NullStringConverter : IValueConverter
+    {
+        readonly string noData = "N/A";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var sourceString = (value ?? "").ToString();
+
+            if (string.IsNullOrWhiteSpace(sourceString))
+            {
+                return noData;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException($"{nameof(NullStringConverter)} cannot be used on TWO-WAY bindings.");
+        }
+    }
+}

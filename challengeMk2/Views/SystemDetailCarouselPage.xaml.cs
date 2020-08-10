2dc7ab0dd9dfb841bc2386cc88c777dd077b3ba3
Amaury Dewaersegger <68230242+AmauryMi8@users.noreply.github.com>
2020-08-10 16:10:54 +0200

    carousel
    init

---
+++ challengeMk2/Views/SystemDetailCarouselPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace challengeMk2.Views
+{
+    public partial class SystemDetailCarouselPage : ContentPage
+    {
+        public SystemDetailCarouselPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

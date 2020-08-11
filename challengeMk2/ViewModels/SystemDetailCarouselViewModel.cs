27c804fae654d1ed085154b7b8f98c9b3677bf3a
Amaury Dewaersegger <68230242+AmauryMi8@users.noreply.github.com>
2020-08-11 10:19:33 +0200

    carousel : retreive data
    retreive data from api, etc. and copy them 3 times (one time per tab) to display details info on separate tab.
    Remove indicator for now because of an error.
    try to update xamarin

--- challengeMk2/ViewModels/SystemDetailCarouselViewModel.cs
+++ challengeMk2/ViewModels/SystemDetailCarouselViewModel.cs
@@ -1,12 +1,141 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using challengeMk2.Models;
+using Newtonsoft.Json;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+
 namespace challengeMk2.ViewModels
 {
     public class SystemDetailCarouselViewModel : BaseViewModel
     {
+        private StarSystem currentSystem;
+        public StarSystem CurrentSystem
+        {
+            get => currentSystem;
+            set
+            {
+                SetProperty<StarSystem>(ref currentSystem, value);
+            }
+        }
+
+        private StarSystem detailedSystem;
+        public StarSystem DetailedSystem
+        {
+            get => detailedSystem;
+            set
+            {
+                SetProperty<StarSystem>(ref detailedSystem, value);
+            }
+        }
+
+
+        private int currentBodyCount;
+        public int CurrentBodyCount
+        {
+            get => currentBodyCount;
+            set
+            {
+                SetProperty<int>(ref currentBodyCount, value);
+            }
+        }
+
+        private double currentDistance;
+        public double CurrentDistance
+        {
+            get => currentDistance;
+            set
+            {
+                SetProperty<double>(ref currentDistance, value);
+            }
+        }
+
+
+        public NetworkAccess CurrentConnectivity { get; set; }
+
+        public IList SystemInfos { get; set; }
+
+
+
+
+
+
+
+
         public SystemDetailCarouselViewModel(StarSystem selectedSystem = null)
         {
             Title = "Star System Details";
+
+            SystemInfos = new ObservableCollection<StarSystem>();
+
+            currentSystem = selectedSystem;
+
+            //Get infos
+            UpdateSystemData();
+        }
+
+
+
+
+
+
+
+
+        private async void UpdateSystemData()
+        {
+            CurrentConnectivity = Connectivity.NetworkAccess;
+
+            //1° Get details from online API
+            if (CurrentConnectivity == NetworkAccess.Internet)
+            {
+                DetailedSystem = await GetDetailsFromApi();
+            }
+            else
+            {
+                DetailedSystem.Name = "No internet connection ! Try again later...";
+                return;
+            }
+
+            //2° Get distance and number of bodies from currentSystem and put them in detailedSystem
+            GetCompInfos();
+
+            //3° Fill collection for Carousel : Copy 3 times detailed system in SystemInfos (one for each tab)
+            FillSystemInfos();
+        }
+
+        private async Task<StarSystem> GetDetailsFromApi()
+        {
+            HttpClient client = new HttpClient();
+
+            // Check system name for special characters : "+" must be replace by "%2b" => Try WebUtility.HtmlEncode(string)
+            string encodedName = WebUtility.UrlEncode(currentSystem.Name);
+
+            string url = $"https://www.edsm.net/api-v1/system?systemName={encodedName}&showInformation=1&showPrimaryStar=1&showPermit=1&showCoordinates=1";
+
+            var response = await client.GetStringAsync(url);
+
+            return JsonConvert.DeserializeObject<StarSystem>(response);
+        }
+
+        private void GetCompInfos()
+        {
+            CurrentBodyCount = currentSystem.BodyCount;
+            CurrentDistance = currentSystem.Distance;
+        }
+
+        private void FillSystemInfos()
+        {
+            SystemInfos.Add(detailedSystem);
+            SystemInfos.Add(detailedSystem);
+            SystemInfos.Add(detailedSystem);
         }
     }
 }

f7fceb940a87270c9b741aed5dca9b1852c25a0f
Amaury Dewaersegger <68230242+AmauryMi8@users.noreply.github.com>
2020-08-12 11:38:37 +0200

    Template selector
    Simplify method with a dictionary <int, datatemplate> and remove switch()

--- challengeMk2/DataSelectors/SystemDetailTemplateSelector.cs
+++ challengeMk2/DataSelectors/SystemDetailTemplateSelector.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.Forms;
 using challengeMk2.Models;
+using System.Collections.Generic;
 
 namespace challengeMk2.DataSelectors
 {
@@ -10,31 +11,20 @@ namespace challengeMk2.DataSelectors
         public DataTemplate SystemInfoTemplate { get; set; }
         public DataTemplate PrimaryStarInfoTemplate { get; set; }
 
+        private Dictionary<int, DataTemplate> InfoTemplates; 
 
         protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
         {
-            //StarSystem currentSystem = ((StarSystem)item);
+            InfoTemplates = new Dictionary<int, DataTemplate>
+            {
+                { 0, GeneralInfoTemplate },
+                { 1, SystemInfoTemplate },
+                { 2, PrimaryStarInfoTemplate }
+            };
+
             StarSystem currentSystem = item as StarSystem;
 
-            //return currentSystem.DataSelectorID switch
-            //{
-            //    0 => GeneralInfoTemplate,
-            //    1 => SystemInfoTemplate,
-            //    2 => PrimaryStarInfoTemplate,
-            //    _ => GeneralInfoTemplate,
-            //};
-
-            switch (currentSystem.DataSelectorID)
-            {
-                case 0:
-                    return GeneralInfoTemplate;
-                case 1:
-                    return SystemInfoTemplate;
-                case 2:
-                    return PrimaryStarInfoTemplate;
-                default:
-                    return GeneralInfoTemplate;
-            }
+            return InfoTemplates[currentSystem.DataSelectorID];
         }
     }
 }

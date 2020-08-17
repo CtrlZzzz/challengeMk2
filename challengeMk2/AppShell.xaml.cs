9cfdb58761faeaa624f82f05e5d17b98035e9896
Amaury <amaury@mi8.be>
2020-08-18 00:29:35 +0200

    Update Theme
    Add Default theme (Dark theme)
    Customize colors

--- challengeMk2/AppShell.xaml.cs
+++ challengeMk2/AppShell.xaml.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-
+﻿
 using Xamarin.Forms;
 
 namespace ChallengeMk2
 {
-    public partial class AppShell : Xamarin.Forms.Shell
+    public partial class AppShell : Shell
     {
         public AppShell()
         {

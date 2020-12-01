5b6ddb09e6f3d7ceb269cb9a774d870722eccd55
Amaury Dewaersegger <amaury@mi8.be>
2020-12-01 12:14:38 +0100

    Init branch
    - add sharedTransition nuget package

--- ChallengeMk2/Views/MainTabbedPage.xaml.cs
+++ ChallengeMk2/Views/MainTabbedPage.xaml.cs
@@ -1,4 +1,6 @@
 using Xamarin.Forms;
+using Plugin.SharedTransitions;
+using System.Diagnostics;
 
 namespace ChallengeMk2.Views
 {
@@ -7,6 +9,21 @@ namespace ChallengeMk2.Views
         public MainTabbedPage()
         {
             InitializeComponent();
+        }
+
+        void SharedTransitionNavigationPage_OnTransitionStarted(object sender, SharedTransitionEventArgs e)
+        {
+            Debug.WriteLine($"From event: Transition started - {e.PageFrom}|{e.PageTo}|{e.NavOperation}");
+        }
+
+        void SharedTransitionNavigationPage_OnTransitionEnded(object sender, SharedTransitionEventArgs e)
+        {
+            Debug.WriteLine($"From event: Transition ended - {e.PageFrom}|{e.PageTo}|{e.NavOperation}");
+        }
+
+        void SharedTransitionNavigationPage_OnTransitionCancelled(object sender, SharedTransitionEventArgs e)
+        {
+            Debug.WriteLine($"From event: Transition cancelled - {e.PageFrom}|{e.PageTo}|{e.NavOperation}");
         }
     }
 }
